e654d970c4daa35635af798837d2951330b446c9
ochamo <60203851+ochamo@users.noreply.github.com>
2023-05-12 21:59:08 -0600

    feat: Add remaining files

--- main.c
+++ main.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/irq.h"
-
+#include "Misc/timer.h"
 
 int main() {
 
     // Initialize standard library
     stdio_init_all();
-
+    
+    initialize_timer();
+    
     while (1) {
+        printf("timer finished");
+        break;
     }
     return 0;
 }
