4324e4b587a7f73dbc76ef5cd88978b58dd0c167
ochamo <60203851+ochamo@users.noreply.github.com>
2023-05-27 16:59:34 -0600

    feat: Finish phase 1

---
+++ Threads/thread.c
@@ -0,0 +1,60 @@
+#include "thread.h"
+
+#include "pico/stdlib.h"
+#include <stdlib.h>
+
+/* Random value for struct thread's `magic' member.
+   Used to detect stack overflow.  See the big comment at the top
+   of thread.h for details. */
+#define THREAD_MAGIC 0xcd6abf4b
+
+#define TIME_SLICE 243 /* Num. of timer ticks to give each thread. */
+static unsigned thread_ticks; /* Num. of timer ticks since last yield.*/
+
+void initialize_scheduler() {
+   queue_init(&wait_list, sizeof(pico_thread), THREAD_LIST_SIZE);
+   queue_init(&ready_list, sizeof(pico_thread), THREAD_LIST_SIZE);
+}
+
+pico_thread* thread_init(void* func, tid_t id, void* thread_args) {
+   pico_thread* t = (pico_thread*)malloc(sizeof(pico_thread));
+   t->func = func;
+   t->status = THREAD_BLOCKED;
+   t->tid = id;
+   return t;
+}
+
+void add_to_wait_list(pico_thread* t) {
+   queue_add_blocking(&wait_list, &t);
+}
+
+void add_to_ready_list(pico_thread* t) {
+   queue_add_blocking(&ready_list, &t);
+}
+
+pico_thread* remove_from_waiting_list() {
+   pico_thread* t;
+   queue_remove_blocking(&wait_list, &t);
+   return t;
+}
+
+pico_thread* remove_from_ready_list() {
+   pico_thread* t;
+   queue_remove_blocking(&ready_list, &t);
+}
+
+void thread_unblock() {
+   pico_thread* t = remove_from_waiting_list();
+   t->status = THREAD_READY;
+   add_to_ready_list(t);
+}
+
+void schedule() {
+   pico_thread* t = remove_from_ready_list();
+   // se ejecuta la función del thread
+   t->status = THREAD_RUNNING;
+   t->func(t->args);
+   t->status = THREAD_DYING;
+   free(t);
+
+}
--- Threads/thread.h
+++ Threads/thread.h
@@ -1,14 +1,49 @@
 #ifndef THREADS_THREAD_H
 #define THREADS_THREAD_H
+#include "pico/util/queue.h"
+#include <stdint.h>
+
+// QUEUE
+static queue_t ready_list;
+static queue_t wait_list;
+// QUEUE
+
+#define THREAD_LIST_SIZE 32
+
 
 /* States in a thread's life cycle. */
-enum thread_status
-  {
+typedef enum thread_status {
     THREAD_RUNNING,     /* Running thread. */
     THREAD_READY,       /* Not running but ready to run. */
     THREAD_BLOCKED,     /* Waiting for an event to trigger. */
     THREAD_DYING        /* About to be destroyed. */
-  };
+} thread_status;
 
+/* Thread identifier type.
+   You can redefine this to whatever type you like. */
+typedef int tid_t;
+#define TID_ERROR ((tid_t) -1)          /* Error value for tid_t. */
+
+typedef struct pico_thread {
+  tid_t tid; // Thread Id
+  thread_status status; // Thread status
+  void (*func)(void*); // Thread function to run
+  void *args; // Threads functions args
+} pico_thread;
+
+void initialize_scheduler(void);
+
+pico_thread* thread_init (void* func, tid_t id, void* thread_args);
+void add_to_wait_list(pico_thread* t);
+pico_thread* remove_from_waiting_list(void);
+pico_thread* remove_from_ready_list(void);
+/*
+  Función que pasa de la wait list a la ready list
+*/
+void thread_unblock();
+void add_to_ready_list(pico_thread* t);
+void thread_start (void);
+
+void schedule(void);
 
 #endif /* threads/thread.h */
